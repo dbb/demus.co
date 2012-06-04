7744a64e548d46cb189b77c08b1611b6b5048b34
Daniel Bolton <dbb008@gmail.com>
2012-06-03 23:33:30 -0400

    Update js/footer.js

--- js/footer.js
+++ js/footer.js
@@ -4,9 +4,9 @@ var ctc = "contact";
 var sep_size = 8;
 var sep = "|";
 
-for ( i=1; i<=sep_size; i++ ) {
-    sep = '&nbsp' + sep + '&nbsp';
-}
+//for ( i=1; i<=sep_size; i++ ) {
+//    sep = '&nbsp' + sep + '&nbsp';
+//}
 
 document.write( abt.link( abt ), sep, ctc.link( ctc ) )
 
