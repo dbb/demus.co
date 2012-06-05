ff2f38c1cc4e11887c5a7d48b34ad1a9f9085bf8
Daniel Bolton <dbb008@gmail.com>
2012-06-04 20:13:35 -0400

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -11,8 +11,7 @@ for ( i=1; i<=sep_size; i++ ) {
     sep = '&nbsp' + sep + '&nbsp';
 }
 
-var follow = '<a href="https://twitter.com/DemusInc" class="twitter-follow-button" data-show-count="false">Follow @DemusInc</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
+var follow = '<a href="https://twitter.com/DemusInc">Follow @DemusInc <img src="/img/twitter_24.png" /></a>';
 
 document.write( abt.link( abt ), sep, ctc.link( "/img/contact.png" ), sep, follow )
 
