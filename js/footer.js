7e9873ea8ffb01262820bef97b1b407eacb8846e
Daniel Bolton <dbb008@gmail.com>
2012-06-04 20:00:46 -0400

    Update js/footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,12 +1,28 @@
 var abt = "about";
 var ctc = "contact";
 
+var fb = "demusinc";
+var tw = "DemusInc";
+
 var sep_size = 8;
 var sep = "|";
 
 for ( i=1; i<=sep_size; i++ ) {
     sep = '&nbsp' + sep + '&nbsp';
 }
+
+!function(d,s,id){
+    var js,fjs=d.getElementsByTagName(s)[0];
+    if (!d.getElementById(id)) {
+        js=d.createElement(s);
+        js.id=id;
+        js.src="https://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js,fjs);
+    }
+
+}
+
+(document,"script","twitter-wjs");
 
 document.write( abt.link( abt ), sep, ctc.link( "/img/contact.png" ) )
 
