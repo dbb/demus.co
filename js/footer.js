40acc232f7a32304f1d5ee3af9b8191622d64a18
Daniel Bolton <dbb008@gmail.com>
2012-06-04 23:48:36 -0400

    Update $_

--- js/footer.js
+++ js/footer.js
@@ -12,7 +12,7 @@ for ( i=1; i<=sep_size; i++ ) {
 }
 
 var follow = '<a href="https://twitter.com/DemusInc">Follow @DemusInc <img src="/img/twitter_24.png" /></a>';
-var like = '<a href="https://facebook.com/DemusInc">Like us<img src="/img/facebook_24.png" /></a>';
+var like = '<a href="https://facebook.com/DemusInc">Like us <img src="/img/facebook_24.png" /></a>';
 
 document.write( abt.link( abt ), sep, ctc.link( "/img/contact.png" ), sep, follow, sep, like )
 
