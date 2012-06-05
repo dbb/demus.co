d3b3e9e5f0bdbabb35a186adc065d8996e2bfd9a
Daniel Bolton <dbb008@gmail.com>
2012-06-04 20:07:54 -0400

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -11,18 +11,8 @@ for ( i=1; i<=sep_size; i++ ) {
     sep = '&nbsp' + sep + '&nbsp';
 }
 
-!function(d,s,id){
-    var js,fjs=d.getElementsByTagName(s)[0];
-    if (!d.getElementById(id)) {
-        js=d.createElement(s);
-        js.id=id;
-        js.src="//platform.twitter.com/widgets.js";
-        fjs.parentNode.insertBefore(js,fjs);
-    }
+var follow = '<a href="https://twitter.com/DemusInc" class="twitter-follow-button" data-show-count="false">Follow @DemusInc</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
 
-}
-
-(document,"script","twitter-wjs");
-
-document.write( abt.link( abt ), sep, ctc.link( "/img/contact.png" ) )
+document.write( abt.link( abt ), sep, ctc.link( "/img/contact.png" ), sep, follow )
 
